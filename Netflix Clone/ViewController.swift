eef1cb114995cd3f1fe253196040d35683c04a45
handyyy <dev.handy.darmawan@gmail.com>
2024-02-26 16:43:38 +0700

    feat: intial setup

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by ndyyy on 26/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+}
+
