0d82c08ca68a5a53743d9f7971edaef27be4efeb
handyyy <dev.handy.darmawan@gmail.com>
2024-03-01 16:15:14 +0700

    docs: test github action

--- Netflix Clone/Views/Upcoming/TitleTableViewCell.swift
+++ Netflix Clone/Views/Upcoming/TitleTableViewCell.swift
@@ -36,9 +36,7 @@ class TitleTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
         setup()
-        
     }
     
     required init?(coder: NSCoder) {
