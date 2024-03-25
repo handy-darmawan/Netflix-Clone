656c71368e0c40be7b1ce4d2350543ac64a42a0b
handyyy <dev.handy.darmawan@gmail.com>
2024-03-25 17:31:09 +0700

    fix: tableviewcell text

--- Netflix Clone/Core/Presentations/TableViewCell/TableViewCell.swift
+++ Netflix Clone/Core/Presentations/TableViewCell/TableViewCell.swift
@@ -11,60 +11,91 @@ import SDWebImage
 class TableViewCell: UITableViewCell {
     static let identifier = "TitleTableViewCell"
     
-    private var upcomingImageView: UIImageView = {
-       let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.clipsToBounds = true
-        return imageView
-    }()
+    //MARK: - Attributes
+    private var upcomingImageView = UIImageView()
+    private var upcomingLabel = UILabel()
+    private var playTitleButton = UIButton()
+    private var movie: Movie?
     
-    private var upcomingLabel: UILabel = {
-         let label = UILabel()
-          label.translatesAutoresizingMaskIntoConstraints = false
-          label.numberOfLines = 0
-          return label
-     }()
-    
-    private var playTitleButton: UIButton = {
-        let button = UIButton()
-        let playImage = UIImage(systemName: "play.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30))
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setImage(playImage, for: .normal)
-        return button
-    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setup()
+        print("init TableViewCell")
+        setups()
     }
+    //TODO: layout subview
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+
+//MARK: Actions
+extension TableViewCell {
+    func setMovie(with movie: Movie) {
+        self.movie = movie
+        configure()
+    }
     
-    private func setup() {
+    private func configure() {
+        guard
+            let movie = movie,
+            let posterPath = movie.posterPath,
+            let title = movie.originalName ?? movie.originalTitle
+        else { return }
+        
+        upcomingLabel.text = title
+        let path = "https://image.tmdb.org/t/p/w500\(posterPath)"
+        upcomingImageView.sd_setImage(with: URL(string: path), completed: nil)
+    }
+}
+
+
+//MARK: Setups
+private extension TableViewCell {
+    func setups() {
+        setupImageView()
+        setupButton()
+        setupLabel()
+    }
+    
+    func setupImageView() {
+        upcomingImageView.translatesAutoresizingMaskIntoConstraints = false
+        upcomingImageView.clipsToBounds = true
         contentView.addSubview(upcomingImageView)
-        contentView.addSubview(upcomingLabel)
-        contentView.addSubview(playTitleButton)
         
         NSLayoutConstraint.activate([
             upcomingImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
             upcomingImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
             upcomingImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
-            upcomingImageView.widthAnchor.constraint(equalToConstant: 100),
-            
-            upcomingLabel.leadingAnchor.constraint(equalTo: upcomingImageView.trailingAnchor, constant: 30),
-            upcomingLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            
-            playTitleButton.leadingAnchor.constraint(equalTo: upcomingLabel.trailingAnchor, constant: 10),
+            upcomingImageView.widthAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    func setupButton() {
+        playTitleButton.translatesAutoresizingMaskIntoConstraints = false
+        let playImage = UIImage(systemName: "play.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30))
+        playTitleButton.setImage(playImage, for: .normal)
+        contentView.addSubview(playTitleButton)
+        
+        NSLayoutConstraint.activate([
+            playTitleButton.topAnchor.constraint(equalTo: contentView.topAnchor),
+            playTitleButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             playTitleButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             playTitleButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15)
         ])
     }
     
-    func configure(with model: Movie) {
-        let path = "https://image.tmdb.org/t/p/w500\(model.posterPath ?? "")"
-        upcomingImageView.sd_setImage(with: URL(string: path), completed: nil)
-        upcomingLabel.text = model.originalName ?? model.originalTitle ?? "Unknown"
+    func setupLabel() {
+        upcomingLabel.translatesAutoresizingMaskIntoConstraints = false
+        upcomingLabel.numberOfLines = 0
+        contentView.addSubview(upcomingLabel)
+        
+        NSLayoutConstraint.activate([
+            upcomingLabel.leadingAnchor.constraint(equalTo: upcomingImageView.trailingAnchor, constant: 30),
+            upcomingLabel.trailingAnchor.constraint(equalTo: playTitleButton.leadingAnchor, constant: -15),
+            upcomingLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
     }
 }
