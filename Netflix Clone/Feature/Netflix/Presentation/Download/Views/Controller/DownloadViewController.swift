3657f18a319b421c422497321bbf843b2c6b49ba
handyyy <dev.handy.darmawan@gmail.com>
2024-03-26 21:11:04 +0700

    feat: add empty state

--- Netflix Clone/Feature/Netflix/Presentation/Download/Views/Controller/DownloadViewController.swift
+++ Netflix Clone/Feature/Netflix/Presentation/Download/Views/Controller/DownloadViewController.swift
@@ -15,8 +15,9 @@ class DownloadViewController: UIViewController {
     
     //MARK: - Attributes
     private var tableView = UITableView()
+    private var emptyStateView = UIView()
+    private var emptyLabel = UILabel()
     private let downloadVM = DownloadViewModel()
-    
     private var dataSource: DataSource?
     
     override func viewDidLoad() {
@@ -27,7 +28,6 @@ class DownloadViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         configureDataSource()
-        
         Task {
             await downloadVM.onLoad()
             DispatchQueue.main.async { [weak self] in
@@ -43,18 +43,35 @@ class DownloadViewController: UIViewController {
 
 
 //MARK: - Actions
-extension DownloadViewController {
-    private func updateSnapshot() {
+private extension DownloadViewController {
+    func updateSnapshot() {
         var snapshot = Snapshot()
         snapshot.appendSections([.download])
         snapshot.appendItems(downloadVM.movies, toSection: .download)
         dataSource?.apply(snapshot)
+        
+        if downloadVM.movies.isEmpty {
+            showEmptyState()
+        }
+        else {
+            hideEmptyState()
+        }
     }
     
-    private func navigateToDetailView(with movie: Movie) {
+    func navigateToDetailView(with movie: Movie) {
         let detailView = DetailView()
         detailView.setMovie(with: movie)
         self.navigationController?.pushViewController(detailView, animated: true)
+    }
+    
+    func showEmptyState() {
+        emptyStateView.isHidden = false
+        emptyLabel.isHidden = false
+    }
+    
+    func hideEmptyState() {
+        emptyStateView.isHidden = true
+        emptyLabel.isHidden = true
     }
 }
 
@@ -65,6 +82,32 @@ private extension DownloadViewController {
         setupNavigationBar()
         setupTableView()
         configureDataSource()
+        setupEmptyState()
+        hideEmptyState()
+    }
+    
+    func setupEmptyState() {
+        emptyStateView.translatesAutoresizingMaskIntoConstraints = false
+        emptyStateView.backgroundColor = .systemBackground
+        view.addSubview(emptyStateView)
+        
+        NSLayoutConstraint.activate([
+            emptyStateView.topAnchor.constraint(equalTo: view.topAnchor),
+            emptyStateView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            emptyStateView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            emptyStateView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        emptyLabel.translatesAutoresizingMaskIntoConstraints = false
+        emptyLabel.text = "No movies downloaded yet!"
+        emptyLabel.font = .systemFont(ofSize: 18, weight: .semibold)
+        emptyLabel.textColor = .label
+        emptyStateView.addSubview(emptyLabel)
+        
+        NSLayoutConstraint.activate([
+            emptyLabel.centerXAnchor.constraint(equalTo: emptyStateView.centerXAnchor),
+            emptyLabel.centerYAnchor.constraint(equalTo: emptyStateView.centerYAnchor)
+        ])
     }
     
     func setupNavigationBar() {
@@ -75,6 +118,7 @@ private extension DownloadViewController {
     func setupTableView() {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.delegate = self
+        tableView.separatorColor = .clear
         tableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.identifier)
         view.addSubview(tableView)
         
@@ -118,7 +162,7 @@ extension DownloadViewController: UITableViewDelegate {
                 self.updateSnapshot()
             }
         }
-        
         return UISwipeActionsConfiguration(actions: [delete])
     }
 }
+
